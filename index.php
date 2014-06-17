8ec019abc6da6e80b7faee4751c7b5308f5e12b1
Tysonzero <danielspencersmith@gmail.com>
2014-06-17 15:38:49 -0700

    Create url function
    Separates URL into 2 parts. For URL
    "http://example.com:8000/example?foo=bar" the first part looks like
    "http://example.com:8000" and the second part looks like
    "/example?foo=bar"

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
+    function url($part)
+    {
+        $url = parse_url($_SERVER['QUERY_STRING']);
+        $scheme = (empty($url['scheme']) ? '' : $url['scheme'].'://');
+        $host = (empty($url['host']) ? '' : $url['host']);
+        $port = (empty($url['port']) ? '' : $url['port']);
+        $path = (empty($url['path']) ? '' : $url['path']);
+        $query = (empty($url['query']) ? '' : '?'.$url['query']);
+        if ($part == 0) {
+            return $scheme.$host.$port;
+        }
+        elseif ($part == 1) {
+            return $path.$query;
+        }
+    };
+
     echo file_get_contents($_SERVER['QUERY_STRING']);
 ?>
