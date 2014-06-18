fd479c99cf1668fa28dfc2063655cc78270515bc
Tysonzero <danielspencersmith@gmail.com>
2014-06-17 18:08:17 -0700

    Add colon before port if one is specified

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         $url = parse_url($_SERVER['QUERY_STRING']);
         $scheme = (empty($url['scheme']) ? '' : $url['scheme'].'://');
         $host = (empty($url['host']) ? '' : $url['host']);
-        $port = (empty($url['port']) ? '' : $url['port']);
+        $port = (empty($url['port']) ? '' : ':'.$url['port']);
         $path = (empty($url['path']) ? '' : $url['path']);
         $query = (empty($url['query']) ? '' : '?'.$url['query']);
         if ($part == 0) {
